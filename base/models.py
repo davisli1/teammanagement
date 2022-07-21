773c7ed87ee4fb52d6a61dd55554c8d75fc3a0f2
Davis Li <davisli1@hotmail.com>
2022-07-21 00:55:29 -0400

    update page styling

--- base/models.py
+++ base/models.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True, blank = True)
-    f_name = models.CharField(max_length = 200, null = True)
-    l_name = models.CharField(max_length = 200, null = True)
+    first_name = models.CharField(max_length = 200, null = True)
+    last_name = models.CharField(max_length = 200, null = True)
     email = models.CharField(max_length = 200, null = True)
     number = models.CharField(max_length = 200, null = True)
     admin = models.BooleanField(default = False)
     created = models.DateTimeField(auto_now_add= True)
 
     def __str__(self):
-        return self.f_name or ''
+        return self.first_name or ''
 
     # when complete it is sent to the bottom
     class Meta:
-        ordering = ['admin']
+        ordering = ['-admin']
