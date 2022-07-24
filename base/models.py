b06fca21a0502de6e1a7d36bd093c922651e0c3f
Davis Li <davisli1@hotmail.com>
2022-07-24 14:57:00 -0400

    set up model and database

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Member(models.Model):
+    user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True, blank = True)
+    first_name = models.CharField(max_length = 200, null = True)
+    last_name = models.CharField(max_length = 200, null = True)
+    email = models.EmailField(max_length = 200, null = True)
+    phone_number = models.CharField(max_length = 200, null = True)
+    admin = models.BooleanField(default = False)
+    created = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return self.first_name 
+
+    # admins get moved up to top of the list
+    class Meta:
+        ordering = ['-admin']
